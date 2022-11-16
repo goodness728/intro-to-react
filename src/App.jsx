34dc6c2514ee094db55c10f4cb167788ca9bdb94
Chelsea Mansoff <chelyrose888@gmail.com>
2022-11-15 21:45:57 -0700

    getRandomNumber

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10);
+}
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">{getRandomNumber()}</header>
     </div>
   );
 }
